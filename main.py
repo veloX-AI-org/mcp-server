7aabf576842c1010b836f4128da86b42dd521792
Ankit Ahirwar <ahirwarankit2345@gmail.com>
2026-02-18 14:33:56 +0530

    MCP Sercver: ENV VARIABLES

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
+from dotenv import load_dotenv
 from fastmcp import FastMCP
+
+load_dotenv()
 
 mcp = FastMCP("velox MCP Server")
 
@@ -10,4 +14,4 @@ def demo_tool():
 # uv sync --frozen
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=3000)
+    mcp.run(transport="http", host="0.0.0.0", port=os.getenv("PORT"))
